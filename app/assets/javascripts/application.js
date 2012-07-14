0591490283c6e9b482fe14a208d8c2252109bc06
rmanusama <rama.manusama@criticube.com>
2012-07-15 01:15:48 +0200

    testing bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-scrollspy
+//= require bootstrap-modal
+//= require bootstrap-dropdown
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Criticube
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # SASS
+    config.sass.preferred_syntax = :sass    
   end
 end
