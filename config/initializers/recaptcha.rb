e2841f387fa6dd64ccedfc77e6fe2bfd2a996675
rmanusama <rama.manusama@criticube.com>
2012-07-16 06:00:44 +0200

    updated recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -3,8 +3,8 @@ Recaptcha.configure do |config|
     config.public_key  = '6Ld5K8wSAAAAAPhuDJi6OaoxUB9cZrwNItFWpYYu'
     config.private_key = '6Ld5K8wSAAAAALEZYW95gHI0ZHkB-pS_wAofn2qA'
   elsif Rails.env.staging?
-    config.public_key  = '6LenQs0SAAAAADOY0jZmyMkxY8ypHbW_Ei0twJD6'
-    config.private_key = '6LenQs0SAAAAADeKbfPGgceY5KjhqvgkjRezPkZO'    
+    config.public_key  = '6Lc6E9QSAAAAAP3xGSDCpys_4ezfPG6I-Si7youd'
+    config.private_key = '6Lc6E9QSAAAAACnjppBJf_GCQuzpwKR40UExU7F5'    
   elsif Rails.env.development? 
     config.public_key  = '6LckaMwSAAAAALEaYZxMRISiig5C5JrWIDJpBTcM'
     config.private_key = '6LckaMwSAAAAAIbNYIERmBFsFW6wjO2KZ-Q_nOmP'
