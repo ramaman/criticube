371369b7971f64b6b9843f70d14e5e84a70bd5bd
rmanusama <rama.manusama@criticube.com>
2012-08-01 15:05:44 +0200

    added shit to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def home
     if user_signed_in?
-      @feed = current_user.dashboard_feed.page(params[:page]).per(50)
+      @feed = current_user.dashboard_feed.page(params[:page]).per(25)
     end
 
     respond_to do |format|
