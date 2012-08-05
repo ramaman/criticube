65c4a8df6f17e98eba5ec4d900dbc8d4724a801f
rmanusama <rama.manusama@criticube.com>
2012-08-05 03:38:46 +0200

    tried setup mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,24 +1,12 @@
 if Rails.env.development?
 
-  # ActionMailer::Base.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings = {
-  #   :address              => "smtp.gmail.com",
-  #   :port                 => 587,
-  #   :domain               => "criticube.com",
-  #   :user_name            => "development@criticube.com",
-  #   :password             => "DoraemonMailer2011!",
-  #   :authentication       => "plain",
-  #   :enable_starttls_auto => true
-  # }  
-  # ActionMailer::Base.default_url_options[:host] = "criticube.com"
-
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address              => "email-smtp.us-east-1.amazonaws.com",
-    :port                 => 25,
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
     :domain               => "criticube.com",
-    :user_name            => "AKIAIQSW7ZVYVWFZHXYA",
-    :password             => "As+3Bt9vpmmHhTq3hjQGsfKdPa6dlz47GiZjDVnAhQmv",
+    :user_name            => "development@criticube.com",
+    :password             => "DoraemonMailer2011!",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }  
@@ -28,15 +16,28 @@ else
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.mandrillapp.com",
-    :port                 => 587,
+    :address              => "email-smtp.us-east-1.amazonaws.com",
+    :port                 => 25,
     :domain               => "criticube.com",
-    :user_name            => "ramaman",
-    :password             => "5d367983-e148-49c2-8758-bc3b91425670",
-    :authentication       => "plain",
+    :user_name            => "AKIAIQSW7ZVYVWFZHXYA",
+    :password             => "As+3Bt9vpmmHhTq3hjQGsfKdPa6dlz47GiZjDVnAhQmv",
+    :authentication => :login,
     :enable_starttls_auto => true
   }  
   ActionMailer::Base.default_url_options[:host] = "criticube.com"
+
+
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   :address              => "smtp.mandrillapp.com",
+  #   :port                 => 587,
+  #   :domain               => "criticube.com",
+  #   :user_name            => "ramaman",
+  #   :password             => "5d367983-e148-49c2-8758-bc3b91425670",
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }  
+  # ActionMailer::Base.default_url_options[:host] = "criticube.com"
 
 end
 # Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
