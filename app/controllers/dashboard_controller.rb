3916c3b3466411dd35943fe4ac06d35de1b5d019
rmanusama <rama.manusama@criticube.com>
2012-08-06 03:07:54 +0200

    enabled static asset serving

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
 
+  before_filter :authenticate_user!, :except => [:home]
+
   def home
     if user_signed_in?
       @feed = current_user.dashboard_feed.page(params[:page]).per(25)
@@ -9,6 +11,18 @@ class DashboardController < ApplicationController
 
     respond_to do |format|
       format.html
+    end    
+  end
+
+  def feed
+    if [:tipe]
+
+    else
+      @feed = current_user.dashboard_feed.page(params[:page]).per(25)
+    end
+
+    respond_to do |format|
+      format.json
     end    
   end
 
