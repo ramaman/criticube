ee165aadcae2524212ce1a5c5d7064432b836e18
rmanusama <rama.manusama@criticube.com>
2012-08-08 05:43:41 +0200

    updated helper

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,10 @@
 module MessagesHelper
+
+  def message_title(user)
+    if user.unread_message_count && (user.unread_message_count > 0)
+      "Messages (#{current_user.unread_messages_count})"
+    else
+      "Messages"
+    end
+  end
 end
