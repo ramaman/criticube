3ce454783425d982b57b58831ff00c96e12e5d95
rmanusama <rama.manusama@criticube.com>
2012-08-08 18:46:48 +0200

    found culprit

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,3 +1,5 @@
-Rack::MiniProfiler.config.skip_paths << '/admin'
+if Rails.env.development?
+  Rack::MiniProfiler.config.skip_paths << '/admin'
+end  
 # Rack::MiniProfiler.config.use_existing_jquery = true
 # Rack::MiniProfiler.config.pre_authorize_cb = lambda {|env| Rails.env.development?}
