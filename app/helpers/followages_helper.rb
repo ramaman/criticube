fe1c76cb89c0f5613a9f6bee0fba7e3857d78a53
rmanusama <rama.manusama@criticube.com>
2012-08-09 14:13:42 +0200

    helper

--- app/helpers/followages_helper.rb
+++ app/helpers/followages_helper.rb
@@ -34,21 +34,21 @@ module FollowagesHelper
   def follow_link(object)
     if !user_signed_in?
       link_to(
-        "Follow #{object.story_name}", 
+        "Follow #{custom_follow_name(object)}", 
         new_user_session_path
       )
     else    
       followage = current_user.following?(object)
       if !followage
         link_to(
-          "Follow #{object.story_name}", 
+          "Follow #{custom_follow_name(object)}", 
           object.follow_permalink,
           :method => :post,
           :remote => :true
         )
       else
         link_to(
-          "Unfollow #{object.story_name}", 
+          "Unfollow #{custom_follow_name(object)}", 
           object.unfollow_permalink,
           :method => :delete,
           :remote => :true, 
@@ -56,6 +56,14 @@ module FollowagesHelper
         )
       end
     end  
+  end
+
+  def custom_follow_name(object)
+    if object.class == User
+      object.first_name
+    else
+      object.story_name  
+    end
   end
 
 end
