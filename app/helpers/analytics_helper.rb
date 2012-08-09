6704e6b211aa3db156a3598e5f45b20041a47962
rmanusama <rama.manusama@criticube.com>
2012-08-09 02:26:32 +0200

    google analytics custom vars

---
+++ app/helpers/analytics_helper.rb
@@ -0,0 +1,19 @@
+module AnalyticsHelper
+
+  def google_analytics_id
+    if Rails.env.production?
+      "UA-20803740-1"
+    else
+      "UA-33989219-1"
+    end
+  end
+
+  def google_analytics_domain
+    if Rails.env.production?
+      "www.criticube.com"
+    else
+      "stage-july14.herokuapp.com"
+    end
+  end
+
+end
