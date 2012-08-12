028a95dec8d81f304cd3720fb9c882c905ceabce
rmanusama <rama.manusama@criticube.com>
2012-08-12 17:28:02 +0200

    added shit

--- db/migrate/20120812014011_add_topic_id_to_cubes.rb
+++ db/migrate/20120812014011_add_topic_id_to_cubes.rb
@@ -1,6 +1,6 @@
 class AddTopicIdToCubes < ActiveRecord::Migration
   def change
-    add_column :cubes, :topic_id
+    add_column :cubes, :topic_id, :integer
     add_index :cubes, :topic_id
   end
 end
