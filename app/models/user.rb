3b1f8a9a2a65760571cb498930e6e43cf23cbb6b
rmanusama <rama.manusama@criticube.com>
2012-08-26 19:28:40 +0700

    added order to cubes

--- app/models/user.rb
+++ app/models/user.rb
@@ -249,7 +249,7 @@ class User < ActiveRecord::Base
   end
 
   def cubes_to_quickpost
-    Cube.includes(:roles).where{|c|(c.featured == true) || ((c.roles.owner_type == 'User') && (c.roles.owner_id == self.id))}
+    Cube.includes(:roles).where{|c|(c.featured == true) || ((c.roles.owner_type == 'User') && (c.roles.owner_id == self.id))}.order('name ASC')
   end
 
   ## User actions
